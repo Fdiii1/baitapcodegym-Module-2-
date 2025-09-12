f034680482c4aecca9bc65736377002c495624af
Huỳnh Tấn Phát <phat76997@gmail.com>
2025-09-13 01:44:54 +0700

    Sử dụng toán tử

---
+++ UseOperator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UseOperator {
+    public static void main(String[] args) {
+        float height;
+        float weight;
+
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println("Nhap vao chieu cao: ");
+        height = Scanner.nextFloat();
+
+        System.out.println("Nhap vao chieu rong: ");
+        weight = Scanner.nextFloat();
+
+        float area = height *weight;
+        System.out.println("ket qua : " + area);
+    }
+}
